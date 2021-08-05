42484f0adf18bed6c1fb575bba5f288c16f869d3
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-08-05 16:23:14 -0400

    Revert back to ugly, arbitrary formatting :P
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- brax/envs/__init__.py
+++ brax/envs/__init__.py
@@ -31,80 +31,80 @@ from brax.envs import ur5e
 from brax.envs import wrappers
 
 _envs = {
-    "fetch": fetch.Fetch,
-    "ant": ant.Ant,
-    "grasp": grasp.Grasp,
-    "halfcheetah": halfcheetah.Halfcheetah,
-    "humanoid": humanoid.Humanoid,
-    "ur5e": ur5e.Ur5e,
-    "reacher": reacher.Reacher,
-    "reacherangle": reacherangle.ReacherAngle,
+  'fetch': fetch.Fetch,
+  'ant': ant.Ant,
+  'grasp': grasp.Grasp,
+  'halfcheetah': halfcheetah.Halfcheetah,
+  'humanoid': humanoid.Humanoid,
+  'ur5e': ur5e.Ur5e,
+  'reacher': reacher.Reacher,
+  'reacherangle': reacherangle.ReacherAngle,
 }
 State = env.State
 Env = env.Env
 
 
 def create(env_name: str, **kwargs) -> Env:
-    """Creates an Env with a specified brax system."""
-    return _envs[env_name](**kwargs)  # type: ignore
+  """Creates an Env with a specified brax system."""
+  return _envs[env_name](**kwargs)  # type: ignore
 
 
 def create_fn(env_name: str, **kwargs) -> Callable[..., Env]:
-    """Returns a function that when called, creates an Env."""
-    return functools.partial(create, env_name, **kwargs)
+  """Returns a function that when called, creates an Env."""
+  return functools.partial(create, env_name, **kwargs)
 
 
 @overload
 def create_gym_env(
-    env_name: str,
-    batch_size: None = None,
-    seed: int = 0,
-    backend: Optional[str] = None,
-    **kwargs
+  env_name: str,
+  batch_size: None = None,
+  seed: int = 0,
+  backend: Optional[str] = None,
+  **kwargs
 ) -> gym.Env:
-    ...
+  ...
 
 
 @overload
 def create_gym_env(
-    env_name: str,
-    batch_size: int,
-    seed: int = 0,
-    backend: Optional[str] = None,
-    **kwargs
+  env_name: str,
+  batch_size: int,
+  seed: int = 0,
+  backend: Optional[str] = None,
+  **kwargs
 ) -> gym.vector.VectorEnv:
-    ...
+  ...
 
 
 def create_gym_env(
-    env_name: str,
-    batch_size: Optional[int] = None,
-    seed: int = 0,
-    backend: Optional[str] = None,
-    **kwargs
+  env_name: str,
+  batch_size: Optional[int] = None,
+  seed: int = 0,
+  backend: Optional[str] = None,
+  **kwargs
 ) -> Union[gym.Env, gym.vector.VectorEnv]:
-    """Creates a `gym.Env` or `gym.vector.VectorEnv` from a Brax environment.
+  """Creates a `gym.Env` or `gym.vector.VectorEnv` from a Brax environment.
 
-    Parameters
-    ----------
-    env_name : str
-        Name of the environment to create.
-    batch_size : Optional[int], optional
-        Number of parallel environments. Defaults to `None`, in which case a single env
-        is returned. When `batch_size` > 1, a subclass of `gym.vector.VectorEnv` is
-        returned instead.
-    seed : int, optional
-        Random seed, by default 0.
-    backend : str, optional
-        Backend used for jit compilation of the `reset` and `step` methods. Defaults to
-        `None`, in which case the backend is chosen automatically.
+  Parameters
+  ----------
+  env_name : str
+    Name of the environment to create.
+  batch_size : Optional[int], optional
+    Number of parallel environments. Defaults to `None`, in which case a single env
+    is returned. When `batch_size` > 1, a subclass of `gym.vector.VectorEnv` is
+    returned instead.
+  seed : int, optional
+    Random seed, by default 0.
+  backend : str, optional
+    Backend used for jit compilation of the `reset` and `step` methods. Defaults to
+    `None`, in which case the backend is chosen automatically.
 
-    Returns
-    -------
-    Union[gym.Env, gym.vector.VectorEnv]
-        A `gym.Env` or a gym.vector.VectorEnv`, depending on the value of `batch_size`.
-    """
-    environment = create(env_name=env_name, batch_size=batch_size, **kwargs)
-    if batch_size:
-        return wrappers.VectorGymWrapper(environment, seed=seed, backend=backend)
-    return wrappers.GymWrapper(environment, seed=seed, backend=backend)
+  Returns
+  -------
+  Union[gym.Env, gym.vector.VectorEnv]
+      A `gym.Env` or a gym.vector.VectorEnv`, depending on the value of `batch_size`.
+  """
+  environment = create(env_name=env_name, batch_size=batch_size, **kwargs)
+  if batch_size:
+    return wrappers.VectorGymWrapper(environment, seed=seed, backend=backend)
+  return wrappers.GymWrapper(environment, seed=seed, backend=backend)
