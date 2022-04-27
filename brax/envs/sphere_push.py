10133a8f79d466e8e4263bfb1c6f2af2f3d20050
Ben Gutteridge <beng@robots.ox.ac.uk>
2022-04-27 16:36:43 +0100

    elasticity back to 1.0, friction doubled to 6.0, angular damping back to -0.05

--- brax/envs/sphere_push.py
+++ brax/envs/sphere_push.py
@@ -102,7 +102,7 @@ class SpherePush(env.Env):
     ctrl_cost = .5 * jp.sum(jp.square(action)) # dependent on torque
     
     # not sure what this is - set to zero for now
-    contact_cost = 0.5 * 1e-3 * jp.sum(jp.square(jp.clip(info.contact.vel, -1, 1))))
+    contact_cost = 0.5 * 1e-3 * jp.sum(jp.square(jp.clip(info.contact.vel, -1, 1)))
     survive_reward = jp.float32(1)
     
     reward = ball_forward_reward 
