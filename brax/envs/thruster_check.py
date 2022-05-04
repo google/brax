5663c1a8f99120f8f1f3557e7b62d5c456f9f59e
BenGutteridge <benpgutteridge@gmail.com>
2022-05-04 12:18:02 +0100

    adding rewards for thruster_check -- should be a full pitm env now

--- brax/envs/thruster_check.py
+++ brax/envs/thruster_check.py
@@ -37,9 +37,12 @@ class ThrusterCheck(env.Env):
     obs = self._get_obs(qp, info)
     reward, done, zero = jp.zeros(3)
     metrics = {
+        'p1_ball_reward': zero,
+        'p2_ball_reward': zero,
+        'piggy_ball_reward': zero,
+        'piggy_touch_ball_cost': zero,
         'reward_ctrl_cost': zero,
         'reward_contact_cost': zero,
-        'reward_forward': zero,
         'reward_survive': zero,
     }
     return env.State(qp, obs, reward, done, metrics)
@@ -48,31 +51,77 @@ class ThrusterCheck(env.Env):
     """Run one timestep of the environment's dynamics."""
 
     # vector piggy to ball
-    v_piggy_ball = state.qp.pos[0,:2] - state.qp.pos[1,:2]
+    ball_pos_before = state.qp.pos[0,:2]
+    piggy_pos_before = state.qp.pos[1,:2]
+    vec_piggy_ball = ball_pos_before - piggy_pos_before
+    piggy_ball_dist_before = norm(vec_piggy_ball)
     piggy_acc = 1. # base piggy acceleration (force/mass)
-    v_piggy_ball /= norm(v_piggy_ball)
-    piggy_acc *= v_piggy_ball # vector of piggy acceleration
+    vec_piggy_ball /= piggy_ball_dist_before
+    piggy_acc *= vec_piggy_ball # vector of piggy acceleration
 
     act = jp.concatenate([piggy_acc, jp.zeros(1), action[:2], jp.zeros(1), action[2:]])
     qp, info = self.sys.step(state.qp, act)
     obs = self._get_obs(qp, info)
 
-    x_before = state.qp.pos[0, 0]
-    x_after = qp.pos[0, 0]
-    forward_reward = (x_after - x_before) / self.sys.config.dt
+    #### REWARDS ####
+    # convention: all terms positive, subtract terms labelled 'cost', add 'reward'
+    # terms:
+    #   `p1_ball_reward`, `p2_ball_reward`  : +ve rewards for players approaching ball
+    #   `piggy_ball_reward`                 : +ve reward for piggy moving away from ball
+    #   `piggy_touch_ball_cost`             : -ve cost for piggy touching ball, end episode
+    #   `ctrl_cost`                         : -ve cost for control (input forces)
+    #   `contact_cost`                      : -ve cost for contact
+    #   `survive_reward`                    : +ve fixed reward for episode not ending
+
+    # Each player move towards ball, small reward
+    scale = 0.1
+    p1_pos_before, p1_pos_after = state.qp.pos[2,:2], qp.pos[2,:2]
+    p2_pos_before, p2_pos_after = state.qp.pos[3,:2], qp.pos[3,:2]
+    ball_pos_after = qp.pos[0,:2]
+    p1_ball_dist_before = norm(ball_pos_before - p1_pos_before)
+    p1_ball_dist_after = norm(ball_pos_after - p1_pos_after)
+    p2_ball_dist_before = norm(ball_pos_before - p2_pos_before)
+    p2_ball_dist_after = norm(ball_pos_after - p2_pos_after)
+    # +ve change, towards ball
+    p1_ball_dist_change = (p1_ball_dist_before - p1_ball_dist_after) / self.sys.config.dt
+    p2_ball_dist_change = (p2_ball_dist_before - p2_ball_dist_after) / self.sys.config.dt
+    p1_ball_reward, p2_ball_reward = p1_ball_dist_change , p2_ball_dist_change
+    p1_ball_reward *= scale
+    p2_ball_reward *= scale
+
+    # Ball move away from piggy, reward
+    scale = 1.0
+    piggy_pos_after = qp.pos[1,:2]
+    piggy_ball_dist_after = norm(ball_pos_after - piggy_pos_after)
+    # +ve means piggy is further away from ball
+    piggy_ball_dist_change = (piggy_ball_dist_after - piggy_ball_dist_before) / self.sys.config.dt
+    piggy_ball_reward = piggy_ball_dist_change * scale
+    
+    # Piggy reach ball, big cost, end episode
+    scale = 100.
+    piggy_touch_ball_cost = (piggy_ball_dist_after < 1.2) * scale
+    done = jp.where(piggy_ball_dist_after < 1.2, 1, 0) # if, then, else
+
+    # standard stuff -- contact cost, survive reward, control cost
     ctrl_cost = .5 * jp.sum(jp.square(action))
     contact_cost = (0.5 * 1e-3 *
                     jp.sum(jp.square(jp.clip(info.contact.vel, -1, 1))))
     survive_reward = jp.float32(1)
-    reward = forward_reward - ctrl_cost - contact_cost + survive_reward
 
-    done = jp.where(qp.pos[0, 2] < 0.2, x=jp.float32(1), y=jp.float32(0))
-    done = jp.where(qp.pos[0, 2] > 1.0, x=jp.float32(1), y=done)
+    # total reward
+    reward = (p1_ball_reward + p2_ball_reward +
+              piggy_ball_reward - piggy_touch_ball_cost - 
+              ctrl_cost - contact_cost + survive_reward)
+
     state.metrics.update(
-        reward_ctrl_cost=ctrl_cost,
-        reward_contact_cost=contact_cost,
-        reward_forward=forward_reward,
-        reward_survive=survive_reward)
+        p1_ball_reward=p1_ball_reward,
+        p2_ball_reward=p2_ball_reward,
+        piggy_ball_reward=piggy_ball_reward,
+        piggy_touch_ball_cost=piggy_touch_ball_cost,
+        ctrl_cost=ctrl_cost,
+        contact_cost=contact_cost,
+        survive_reward=survive_reward,
+    )
 
     return state.replace(qp=qp, obs=obs, reward=reward, done=done)
 
@@ -82,31 +131,13 @@ class ThrusterCheck(env.Env):
 
   def _get_obs(self, qp: brax.QP, info: brax.Info) -> jp.ndarray:
     """Observe ant body position and velocities."""
-    # # some pre-processing to pull joint angles and velocities
-    # (joint_angle,), (joint_vel,) = self.sys.joints[0].angle_vel(qp)
 
-    # # qpos:
-    # # Z of the torso (1,)
-    # # orientation of the torso as quaternion (4,)
-    # # joint angles (8,)
-    # qpos = [qp.pos[0, 2:], qp.rot[0], joint_angle]
+    #### OBSERVATIONS ####
+    all_body_pos = qp.pos[:-1,:2] # x,y positions of both players, ball and piggy (8,)
+    all_body_vel = qp.vel[:-1,:2] # x,y velocities of both players, ball and piggy (8,)
+    ball_ang  = qp.ang[0]         # ball angular velocities (3,)
 
-    # # qvel:
-    # # velocity of the torso (3,)
-    # # angular velocity of the torso (3,)
-    # # joint angle velocities (8,)
-    # qvel = [qp.vel[0], qp.ang[0], joint_vel]
-
-    # # external contact forces:
-    # # delta velocity (3,), delta ang (3,) * 10 bodies in the system
-    # # Note that mujoco has 4 extra bodies tucked inside the Torso that Brax
-    # # ignores
-    # cfrc = [jp.clip(info.contact.vel, -1, 1), jp.clip(info.contact.ang, -1, 1)]
-    # # flatten bottom dimension
-    # cfrc = [jp.reshape(x, x.shape[:-2] + (-1,)) for x in cfrc]
-
-
-    return jp.concatenate([qp.pos[0]] + [qp.vel[0]] + [qp.rot[0]] + [qp.ang[0]])
+    return jp.concatenate([all_body_pos] + [all_body_vel] + [ball_ang])
 
 
 _SYSTEM_CONFIG = """
