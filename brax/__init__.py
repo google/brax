ef9c1d1eb666c947103761c7e5942f8c71f82f3d
Brax Team <no-reply@google.com>
2023-03-21 12:53:36 -0400

    Internal change
    PiperOrigin-RevId: 518302731
    Change-Id: Idf32aa028ab2904baa5ebfa655a456e7d9881255

--- brax/__init__.py
+++ brax/__init__.py
@@ -14,7 +14,7 @@
 
 """Import top-level classes and functions here for encapsulation/clarity."""
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from brax.physics.base import Info
 from brax.physics.base import QP
