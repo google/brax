c9021f2ae55c4c1d2c9b777d58479e4aeef43fd6
Erik Frey <erikfrey@google.com>
2025-06-13 11:09:16 -0700

    Internal change
    PiperOrigin-RevId: 771157415
    Change-Id: I07b3c7fa151661a7b6cbb67374d96f0f50a40c4f

--- brax/__init__.py
+++ brax/__init__.py
@@ -14,7 +14,7 @@
 
 """Import top-level classes and functions here for encapsulation/clarity."""
 
-__version__ = '0.12.3'
+__version__ = '0.12.4'
 
 from brax.base import Motion
 from brax.base import State
